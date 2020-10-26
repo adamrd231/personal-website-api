667f9c2b2e267960b053bfcb5022f20134eef53f
adamrd231 <adam.rdconcepts@gmail.com>
2020-10-26 15:20:52 -0700

    Create API Database to hold projects, blogs and other needed information for personal portfolio website

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -39,17 +39,24 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'projects',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "https://localhost:8000",
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'personal_portfolio.urls'
