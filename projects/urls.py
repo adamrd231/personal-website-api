5ab8196928403a882940d41eb5f043f9383e4169
adamrd231 <adam.rdconcepts@gmail.com>
2020-11-17 00:56:19 -0800

    Update settings

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,7 @@ from django.urls import path;
 from rest_framework import routers;
 from django.conf.urls import include;
 from .views import ProjectViewSet, PhotoViewSet, CategoryViewSet, BlogViewSet, QuoteViewSet;
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 router = routers.DefaultRouter()
 router.register('projects', ProjectViewSet)
@@ -11,7 +12,12 @@ router.register('categories', CategoryViewSet)
 router.register('blog', BlogViewSet)
 router.register('quotes', QuoteViewSet)
 
-urlpatterns = [
-    path('', include(router.urls)),
+
     path('summernote/', include('django_summernote.urls')),
 ] 
+
+url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root':"path\to\your\static\folder"}),
+
+urlpatterns = [
+    path('', include(router.urls)),
+urlpatterns += staticfiles_urlpatterns()
