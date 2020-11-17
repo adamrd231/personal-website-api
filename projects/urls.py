ebd4371da42c378310e8ebf3506f2319958fc0ac
adamrd231 <adam.rdconcepts@gmail.com>
2020-11-17 01:12:08 -0800

    Prepare for heroku

--- projects/urls.py
+++ projects/urls.py
@@ -3,8 +3,6 @@ from django.urls import path;
 from rest_framework import routers;
 from django.conf.urls import include;
 from .views import ProjectViewSet, PhotoViewSet, CategoryViewSet, BlogViewSet, QuoteViewSet;
-import django.views.static
-
 
 router = routers.DefaultRouter()
 router.register('projects', ProjectViewSet)
@@ -13,13 +11,8 @@ router.register('categories', CategoryViewSet)
 router.register('blog', BlogViewSet)
 router.register('quotes', QuoteViewSet)
 
-
-
 urlpatterns = [
     path('', include(router.urls)),
     path('summernote/', include('django_summernote.urls')),
+    
 ] 
-
-urlpatterns += [
-   url(r'^static/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.STATIC_ROOT, 'show_indexes': settings.DEBUG})
-]
