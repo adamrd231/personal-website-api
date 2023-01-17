9ad62be9bd78f1791ee851401ab217f72095266f
Adam Reed <adam.rdconcepts@gmail.com>
2023-01-16 20:43:44 -0700

    Update allowed hosts

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1',' https://website-portfolio-rdconcepts.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1',' https://website-portfolio-rdconcepts.herokuapp.com', 'website-portfolio-rdconcepts.herokuapp.com']
 
 
 # Application definition
